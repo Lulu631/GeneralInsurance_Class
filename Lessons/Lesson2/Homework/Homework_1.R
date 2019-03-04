229e7041a1777273b156268536ddab66025dabc0
Lulu631 <47779378+Lulu631@users.noreply.github.com>
2019-03-04 22:33:59 +0100

    Update Homework_1.R

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,7 +3,24 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
-# 
+# install.packages("dplyr")
+library(dplyr)
+
+setwd("C:/Users/userPC/Documents/GeneralInsurance_Class/Data")
+dt_KPI <- read.csv("lesson2_KPI.csv")
+
+dt_KPI %>% group_by(Unit) %>% summarize(Premium = sum(Premium, na.rm = TRUE)) %>% arrange(desc(Premium)) #Najvacsi profit ma Unit 7
+
+dt_KPI %>% filter(Unit == "Unit7")  %>% mutate(profit=Premium-Expenses-Losses) %>% 
+  group_by(Year) %>% summarize(profit=sum(profit, na.rm = TRUE)) %>% arrange(profit)   # najhorsi bol rok 2014
+
+#install.packages("ggplot2")
+library(ggplot2)
+
+dt_KPI %>% filter(Unit == "Unit7")  %>% mutate(profit=Premium-Expenses-Losses) %>% 
+  group_by(Year) %>% summarize(profit=sum(profit, na.rm = TRUE)) %>% arrange(profit) %>% ggplot(aes(x = Year, y = profit)) + geom_col()
+
+
 # 
 # 
 # 
@@ -29,6 +46,6 @@
 
 
 # Your Explanation about analysis:
-# 
-# 
-# 
+# 1. zistenie portfolia s najvacsim profitom (nie som si ista, co je v tomto pripade portfolio - brala som stlpec Unit)
+# 2. pomocou filtrovania dat pre Unit 7 zistenie najhorsieho roku
+# 3. graf - vidime z neho, ze naozaj je najhorsi rok 2014
